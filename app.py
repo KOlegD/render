ea4b02b71558c2f15a3a51510f6ad1e5566de79d
Казаков Олег Дмитриевич <43730369+KOlegD@users.noreply.github.com>
2025-03-25 13:27:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Привет от приложения Flask</h1>"
+    return "<h1>Привет от приложения Flask1</h1>"
 
 
 if __name__ == '__main__':
